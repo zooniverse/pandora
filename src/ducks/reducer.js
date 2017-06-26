c78074bc3fca3a7c00c5a716c11d24e0a9f6997c
Jim O'Donnell <jim@zooniverse.org>
2017-06-26 11:11:03 +0100

    Remove old contents reducer and actions

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import contents from './contents';
 import login from './login';
 import project from './project';
 import projects from './projects';
 import resource from './resource';
 
 export default combineReducers({
-  contents,
   login,
   project,
   projects,
