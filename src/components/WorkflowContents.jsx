04b0d65d59f6e2a8f381b0d6b3aa4d60c503935d
Jim O'Donnell <jim@zooniverse.org>
2017-07-11 12:35:57 +0100

    Show translations for workflow contents

--- src/components/WorkflowContents.jsx
+++ src/components/WorkflowContents.jsx
@@ -13,12 +13,17 @@ options.log = (test) => {
 function WorkflowContents(props) {
   const { contents } = props;
   const workflow_contents = contents.original || { strings: {} };
+  const translation = contents.translation || { strings: {} };
   const strings = [];
   const keys = workflow_contents.strings ? Object.keys(workflow_contents.strings) : [];
-  keys.map(key => strings.push(<p key={key}><b>{key}</b> {workflow_contents.strings[key]}</p>));
+  keys.map(key => {
+    strings.push(<p key={key}><b>{key}</b> {workflow_contents.strings[key]}</p>);
+    strings.push(<p key={`translation-${key}`}><b>Translation</b> {translation.strings[key]}</p>);
+  });
   return (
     <div>
       <h2>Workflow Contents</h2>
+      <p>Language: {translation.language}</p>
       {strings}
     </div>
   );
