877818013a3e90d42f6dfba97dce78d4057b5955
Jim O'Donnell <jim@zooniverse.org>
2017-07-18 13:00:28 +0100

    Language Selector component Extract all the language selection bits into their own component.

--- src/containers/ProjectDashboardContainer.jsx
+++ src/containers/ProjectDashboardContainer.jsx
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import languages from '../constants/languages';
+import LanguageSelector from '../components/LanguageSelector';
 import { fetchProject } from '../ducks/project';
 import { createTranslation } from '../ducks/resource';
-import Select from 'grommet/components/Select';
 
 const propTypes = {
   actions: PropTypes.object.isRequired,
@@ -30,95 +29,35 @@ const propTypes = {
 class ProjectDashboardContainer extends Component {
   constructor() {
     super();
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.onLanguageChange = this.onLanguageChange.bind(this);
+    this.onChangeLanguage = this.onChangeLanguage.bind(this);
     this.state = {
       searchText: '',
-      option: {},
+      option: {
+        label: '',
+        value: ''
+      },
     };
   }
+
   componentDidMount() {
     const { actions } = this.props;
     actions.fetchProject(this.props.params.project_id);
   }
 
-  handleSearch(event) {
-    this.setState({
-      searchText: event.target.value,
-    });
-  }
-
-  handleSelect({ option }) {
-    const { actions } = this.props;
-    this.setState({
-      option,
-    });
-  }
-
-  onLanguageChange(e) {
-    const { value } = e.target;
-    const [option] = languages.filter(option => option.value === value);
-    this.handleSelect({ option });
-  }
-
-  getSelectOptions() {
-    const searchStr = this.state.searchText;
-    let langs = languages.slice();
-    if (searchStr) {
-      const matchesSearch = (langObj) => langObj.label.toLowerCase().substr(0, searchStr.length) === searchStr;
-      langs = languages.filter(matchesSearch);
-    }
-    return langs;
+  onChangeLanguage(option) {
+    this.setState({ option });
   }
 
   render() {
     const project = this.props.project.data;
     const { fieldguides, pages, workflows, tutorials } = this.props.project;
     const { translations } = this.props.resource;
-    let newLanguages = this.getSelectOptions();
     const language = this.state.option.value;
-    const existingLanguages = [];
-    translations.map((translation) => {
-      const [languageOption] = languages.filter(option => option.value === translation.language);
-      existingLanguages.push(languageOption);
-      newLanguages = newLanguages.filter(option => option !== languageOption);
-    });
-    const selectedOption = newLanguages.indexOf(this.state.option) > -1 ? this.state.option : null;
     
     return (
       <div>
         <h2>Project Dashboard</h2>
-        <h3>Pick a language</h3>
-        <ul>
-          {existingLanguages.map((option) => {
-            return (
-              <li key={option.value}>
-                <label>
-                  <input
-                    name="lang"
-                    type="radio"
-                    checked={option.value === this.state.option.value}
-                    value={option.value}
-                    onChange={this.onLanguageChange}
-                  />
-                  {option.label}
-                </label>
-              </li>
-            );
-          })}
-        </ul>
-        <label>
-          or add a new language
-          <Select
-            onChange={this.handleSelect}
-            onSearch={this.handleSearch}
-            options={newLanguages}
-            placeHolder="Select a language"
-            value={selectedOption}
-          />
-        </label>
-
+        <LanguageSelector translations={translations} value={this.state.option} onChange={this.onChangeLanguage} />
         {project.primary_language && React.cloneElement(this.props.children, { fieldguides, language, pages, project, tutorials, workflows })}
       </div>
     );
