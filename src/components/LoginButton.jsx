f8a0efc94df55ecb4cfd43954ee07a79b98a1f59
Jim O'Donnell <jim@zooniverse.org>
2017-08-08 14:35:56 +0100

    Fixup block statements

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { DefaultButton } from 'pui-react-buttons';
 
-const LoginButton = ({ login }) => {
-  return (
+const LoginButton = ({ login }) =>
+  (
     <DefaultButton type="submit" onClick={login}>Login</DefaultButton>
   );
-};
 
 LoginButton.propTypes = {
   login: PropTypes.func.isRequired
--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { DefaultButton } from 'pui-react-buttons';
 
-const LogoutButton = ({ logout, user }) => {
-  return (
+const LogoutButton = ({ logout, user }) =>
+  (
     <div className="logout-button">
       <DefaultButton type="submit" onClick={logout}>Logout {user.login}</DefaultButton>
     </div>
   );
-};
 
 LogoutButton.propTypes = {
   logout: PropTypes.func.isRequired,
