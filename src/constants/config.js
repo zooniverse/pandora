16f6e1315e670413d43134c0bdc35061a441ca24
Jim O'Donnell <jim@eatyourgreens.org.uk>
2017-11-15 13:30:04 +0000

    Add staging config (#51)

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,7 @@
 const config = {
+  staging: {
+    panoptesAppId: '30963426190f0b1306331e603368527f3d0650920d5e1c9b6287ad09fdd3231e' // Panoptes staging API
+  },
   development: {
     panoptesAppId: '30963426190f0b1306331e603368527f3d0650920d5e1c9b6287ad09fdd3231e' // Panoptes staging API
   },
