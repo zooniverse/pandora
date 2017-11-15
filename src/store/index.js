ba1372bd24eb2e57daefbed9694cf13bbac0ab93
Jim O'Donnell <jim@eatyourgreens.org.uk>
2017-11-15 15:17:12 +0000

    Remove redux-logger (#53)

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from '../ducks/reducer';
 
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware,
-  createLogger(),
 )(createStore);
 
 export default function configureStore(initialState) {
