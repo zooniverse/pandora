20dac9046186310eb2c5d0fa0a0fe99b2b19d64a
Jim O'Donnell <jim@zooniverse.org>
2018-05-08 15:47:37 +0100

    Fix tests Install JSDOM. Mock DOM environment for tests.

---
+++ test/setup.js
@@ -0,0 +1,26 @@
+import { JSDOM } from 'jsdom';
+import { URL } from 'whatwg-url';
+
+// Set up fake DOM for use by Enzyme's mount() method.
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js'
+};
+global.screen = window.screen;
+global.pageXOffset = window.pageXOffset;
+global.pageYOffset = window.pageYOffset;
+global.URL = URL;
+global.HTMLElement = window.HTMLElement; // to get chai's deep equality to work
+
+copyProps(window, global);
