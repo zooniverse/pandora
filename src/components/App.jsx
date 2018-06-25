2b2f0eea209b1a0512af911e986cc341f64808a7
Jim O'Donnell <jim@eatyourgreens.org.uk>
2018-06-25 16:49:10 +0100

    Check for a logged-in user (#85)
    Rewrite App as a pure function, connected to the redux store.
    Only show the translations editor to logged-in users.
    Display a message otherwise.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 import AuthContainer from '../containers/AuthContainer';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <header className="site-header">
-          <h1 className="title">Pandora</h1>
-          <Link to="/about" className="link">About</Link>
-          <AuthContainer />
-        </header>
-        <section className="content-section">
-          {this.props.children || 'It unleashes translations on Panoptes.'}
-        </section>
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div>
+      <header className="site-header">
+        <h1 className="title">Pandora</h1>
+        <Link to="/about" className="link">About</Link>
+        <AuthContainer />
+      </header>
+      <section className="content-section">
+        {props.user ? props.children : <p>You must be logged in to edit translations.</p>}
+      </section>
+    </div>
+  );
 }
 
 App.propTypes = {
   children: PropTypes.node,
+  user: PropTypes.shape({
+    id: PropTypes.string
+  })
 };
 
-export default App;
+App.defaultProps = {
+  children: null,
+  user: null
+};
+
+const mapStateToProps = state => ({
+  user: state.login.user,
+  initialised: state.login.initialised
+});
+
+export default connect(mapStateToProps)(App);
+export { App };
+
