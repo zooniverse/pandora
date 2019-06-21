603ba3eecb3ad0a05bc3fa3353d8bd98a8f574d9
Jim O'Donnell <jim@zooniverse.org>
2019-06-21 17:19:16 +0100

    Wire up the organisation reducers

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
 import login from './login';
+import organisation from './organisation';
+import organisations from './organisations';
 import project from './project';
 import projects from './projects';
 import resource from './resource';
 
 export default combineReducers({
   login,
+  organisation,
+  organisations,
   project,
   projects,
   resource
