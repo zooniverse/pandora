58d9fb5e77b9558895f8d4341dcf3061b52c97b2
Jim O'Donnell <jim@zooniverse.org>
2019-06-24 13:57:17 +0100

    npm update

--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,7 @@ import { JSDOM } from 'jsdom';
 import { URL } from 'whatwg-url';
 
 // Set up fake DOM for use by Enzyme's mount() method.
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost' });
 const { window } = jsdom;
 
 function copyProps(src, target) {
