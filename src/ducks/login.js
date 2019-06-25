b4e37365fe2990127017e3f0e431f7d45dd04513
Jim O'Donnell <jim@zooniverse.org>
2019-06-25 15:52:30 +0100

    Add an admin toggle Add an admin toggle to the header, for admin users. Add admin=true to all API requests when the toggle is on.

--- src/ducks/login.js
+++ src/ducks/login.js
@@ -2,20 +2,28 @@ import oauth from 'panoptes-client/lib/oauth';
 
 // Action Types
 const SET_LOGIN_USER = 'project/user/SET_LOGIN_USER';
+const SET_ADMIN_MODE = 'project/user/SET_ADMIN_MODE';
 
 // Reducer
 const initialState = {
+  adminMode: false,
   user: null,
   initialised: false
 };
 
 function loginReducer(state = initialState, action) {
   switch (action.type) {
-    case SET_LOGIN_USER:
+    case SET_LOGIN_USER: {
       return {
+        adminMode: false,
         user: action.user,  // null if logged out.
         initialised: true  // true once we know if user is logged in/out; false if unknown.
       };
+    }
+    case SET_ADMIN_MODE: {
+      const { adminMode } = action;
+      return Object.assign({}, state, { adminMode });
+    }
     default:
       return state;
   }
@@ -55,6 +63,13 @@ function setLoginUser(user) {
   };
 }
 
+function setAdminMode(adminMode) {
+  return {
+    type: SET_ADMIN_MODE,
+    adminMode
+  };
+}
+
 // Helper functions
 function computeRedirectURL(window) {
   const { location } = window;
@@ -69,5 +84,6 @@ export {
   checkLoginUser,
   loginToPanoptes,
   logoutFromPanoptes,
-  setLoginUser
+  setLoginUser,
+  setAdminMode
 };
